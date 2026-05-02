83613a3d2d68e531ccacdba1578008ab40566293
Ganga18062001 <gangashirale@gmail.com>
2026-05-02 17:07:05 +0530

    Practice on vector class

--- src/linkList/Demo.java
+++ src/linkList/Demo.java
@@ -1,7 +1,20 @@
 package linkList;
 
+import java.util.LinkedList;
+
 public class Demo {
     public static void main(String[] args) {
+
+        LinkedList lll = new LinkedList();
+        lll.add(101);
+        lll.add("ram");
+        lll.add('c');
+        lll.addFirst(100);
+        lll.addLast(104);
+
+        System.out.println(lll);
+        System.out.println(lll.getFirst());
+        System.out.println(lll.getLast());
 
 
 
