3f5f4bbb80c55f1f90ec77916ce2e8bb1d058d99
Ganga18062001 <gangashirale@gmail.com>
2026-05-07 18:41:14 +0530

    Resolved conflict in Git.java

--- src/gitPrc/Git.java
+++ src/gitPrc/Git.java
@@ -3,5 +3,6 @@ package gitPrc;
 public class Git {
     public static void main(String[] args) {
         System.out.println("practice on git");
+//        System.out.println("git reset");
     }
 }
